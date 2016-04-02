67b37db7818a4b965edd738928d48e248cdc6a35
nineonine <mail4chemik@gmail.com>
2016-04-01 18:38:26 -0700

    start building API actions

---
+++ src/Discogs/Actions/Database.hs
@@ -0,0 +1,32 @@
+module Discogs.Actions.Database where
+
+import Discogs.Types.Discogs
+import Discogs.Types.Release
+import Discogs.Types.Release.Master
+import Discogs.Types.Release.Versions
+import Discogs.Types.Artist
+import Discogs.Types.Label
+import qualified Discogs.Types.Artist.Release as AR
+import qualified Discogs.Types.Label.Release as LR
+import qualified Discogs.Request.Database as Req
+
+release :: Monad m => Int -> DiscogsT m Release
+release n = runRequest $ Req.getRelease n
+
+artist :: Monad m => Int -> DiscogsT m Artist
+artist n = runRequest $ Req.getArtist n
+
+master :: Monad m => Int -> DiscogsT m Master
+master n = runRequest $ Req.getMaster n
+
+label :: Monad m => Int -> DiscogsT m Label
+label n = runRequest $ Req.getLabel n
+
+releaseVersions :: Monad m => Int -> DiscogsT m Versions
+releaseVersions n = runRequest $ Req.getReleaseVersions n
+
+artistReleases :: Monad m => Int -> DiscogsT m AR.ArtistReleases
+artistReleases n = runRequest $ Req.getArtistReleases n
+
+labelReleases :: Monad m => Int -> DiscogsT m LR.LabelReleases
+labelReleases n = runRequest $ Req.getLabelReleases n
